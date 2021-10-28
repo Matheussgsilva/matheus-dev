008af943af0555b767c314b765615e9ab3fa4ef9
Matheussgsilva <matheussgsilva@hotmail.com>
2021-10-28 16:35:24 -0300

    Change nav style on scroll

--- src/script/style.js
+++ src/script/style.js
@@ -2,10 +2,8 @@
 window.onscroll = scrollShowNav;
 function scrollShowNav() {
    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementsByClassName("header")[0].style.padding = "30px 50px 20px";
       document.getElementsByClassName("header")[0].style.backgroundColor = "black";
    } else {
-      document.getElementsByClassName("header")[0].style.padding = "40px 60px 30px";
       document.getElementsByClassName("header")[0].style.backgroundColor = "transparent";
    }
 }
