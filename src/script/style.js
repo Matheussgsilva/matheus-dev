674f7b5412c87f98de4f799cf77d128fdd134465
Matheussgsilva <matheussgsilva@hotmail.com>
2021-10-30 13:55:14 -0300

    Adds scroll effect on skill-bar

--- src/script/style.js
+++ src/script/style.js
@@ -6,4 +6,24 @@ function scrollShowNav() {
    } else {
       document.getElementsByClassName("header")[0].style.backgroundColor = "transparent";
    }
+   if (document.body.scrollTop > 1290 || document.documentElement.scrollTop > 1290) {
+      document.getElementById("skill-bar-html").style.width = "150px";
+   } else {
+      document.getElementById("skill-bar-html").style.width = "0px";
+   }
+   if (document.body.scrollTop > 1390 || document.documentElement.scrollTop > 1390) {
+      document.getElementById("skill-bar-css").style.width = "100px";
+   } else {
+      document.getElementById("skill-bar-css").style.width = "0px";
+   }
+   if (document.body.scrollTop > 1490 || document.documentElement.scrollTop > 1490) {
+      document.getElementById("skill-bar-js").style.width = "100px";
+   } else {
+      document.getElementById("skill-bar-js").style.width = "0px";
+   }
+   if (document.body.scrollTop > 1590 || document.documentElement.scrollTop > 1590) {
+      document.getElementById("skill-bar-react").style.width = "80px";
+   } else {
+      document.getElementById("skill-bar-react").style.width = "0px";
+   }
 }
