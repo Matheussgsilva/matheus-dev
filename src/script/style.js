442e11eb89eab9914055e589568a4781ea0a118a
Matheussgsilva <matheussgsilva@hotmail.com>
2021-11-01 12:10:19 -0300

    Change the upscroll value

--- src/script/style.js
+++ src/script/style.js
@@ -6,22 +6,22 @@ function scrollShowNav() {
    } else {
       document.getElementsByClassName("header")[0].style.backgroundColor = "transparent";
    }
-   if (document.body.scrollTop > 1280 || document.documentElement.scrollTop > 1280) {
+   if (document.body.scrollTop > 1585 || document.documentElement.scrollTop > 1585) {
       document.getElementById("skill-bar-html").style.width = "150px";
    } else {
       document.getElementById("skill-bar-html").style.width = "0px";
    }
-   if (document.body.scrollTop > 1380 || document.documentElement.scrollTop > 1380) {
+   if (document.body.scrollTop > 1685 || document.documentElement.scrollTop > 1685) {
       document.getElementById("skill-bar-css").style.width = "100px";
    } else {
       document.getElementById("skill-bar-css").style.width = "0px";
    }
-   if (document.body.scrollTop > 1480 || document.documentElement.scrollTop > 1480) {
+   if (document.body.scrollTop > 1785 || document.documentElement.scrollTop > 1785) {
       document.getElementById("skill-bar-js").style.width = "100px";
    } else {
       document.getElementById("skill-bar-js").style.width = "0px";
    }
-   if (document.body.scrollTop > 1580 || document.documentElement.scrollTop > 1580) {
+   if (document.body.scrollTop > 1885 || document.documentElement.scrollTop > 1885) {
       document.getElementById("skill-bar-react").style.width = "80px";
    } else {
       document.getElementById("skill-bar-react").style.width = "0px";
